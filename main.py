de2435ce82acb027db4d6fb0d661152c31eb27ce
Stephanny Grilanda <stephanny.grilanda@gmail.com>
2026-06-15 21:36:28 -0300

    adição novos arquivos e padronização de erros

--- main.py
+++ main.py
@@ -1,23 +1,67 @@
 # importação libs
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from dotenv import load_dotenv
-import os
-import domain
-from infra import security
+from fastapi.responses import JSONResponse
+from datetime import datetime, timezone
+
 
 # importação rotas da api
 
 from api.routes import auth_routes
 from api.routes import order_routes
+from api.routes import payment_routes
 
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(
+    title="API Raízes do Nordeste",
+    description="API para o projeto Raízes do Nordeste, uma rede de restaurantes com comidas típicas do Nordeste.",
+    version="1.0.0",
+)
+
+def _mapear_codigos_erro(status_code: int) -> str:
+    """Função para mapear e padronizar mensagens de erro de acordo com o status HTTP."""
+    codigos_erro = {
+        400: "REQUISIÇÃO INVALIDA",
+        401: "USUARIO NÃO AUTENTICADO",
+        403: "ACESSO NEGADO",
+        404: "RECURSO NÃO ENCONTRADO",
+        409: "CONFLITO DE RECURSO",
+        422: "ERRO DE VALIDACAO",
+    }
+
+    return codigos_erro.get(status_code, "ERRO INTERNO")
+
+@app.exception_handler(HTTPException)
+async def tratar_excecao(request: Request, exc: HTTPException):
+    """Garante que as mensagens de erro retorne um json padronizado."""
+    if isinstance(exc.detail, dict):
+        codigo_erro = exc.detail.get("codigo_erro", _mapear_codigos_erro(exc.status_code))
+        mensagem = exc.detail.get("mensagem", "Ocorreu um erro durante a requisição.")
+        detalhes = exc.detail.get("detalhes", [])
+    else:
+        codigo_erro = _mapear_codigos_erro(exc.status_code)
+        mensagem = str(exc.detail) if exc.detail else "Ocorreu um erro durante a requisição."
+        detalhes = []
+    
+    return JSONResponse(
+        status_code = exc.status_code,
+        content = {
+            "error": {
+                "error": codigo_erro,
+                "message": mensagem,
+                "details": detalhes,
+                "timestamp": datetime.now(timezone.utc).isoformat(),
+                "path": str(request.url.path)
+            },
+        }
+    )
 
 app.include_router(auth_routes.auth_router)
 app.include_router(order_routes.order_router)
+app.include_router(payment_routes.payment_router)
 
 @app.get("/")
 def root():
-    return {"message": "Bem-vindo ao Raízes do Nordeste!"}
+    return {"message": "Projeto Raízes do Nordeste! Acesse /docs para consultar a documentação da API."}
